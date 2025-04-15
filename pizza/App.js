051ece14bdd64948d5ed9cfc290b830ec97fb8e6
roypsetec <roypstcc@gmail.com>
2025-04-15 14:41:43 -0300

    atualização botao

--- pizza/App.js
+++ pizza/App.js
@@ -1,14 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import Ionicons from '@expo/vector-icons/Ionicons';
+import { ImageBackground, StyleSheet, Text, View } from 'react-native';
 import FontAwesome5 from '@expo/vector-icons/FontAwesome5';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <FontAwesome5 name="pizza-slice" size={24} color="black" />
-      <Text>Pizzaria Presunto's</Text>
-      
+
+      <View style={styles.logo}>
+      <FontAwesome5 name="pizza-slice" size={20} color="#ffffff" />
+      <Text style={styles.text}>Pizzaria Presunto</Text>
+      </View>
+
+      <ImageBackground
+      source={require('./assets/pizza.jpg')} 
+      style={styles.imagem}
+      resizeMode='stretch'>
+      </ImageBackground>
+
+      <View style={[styles.botoes, {backgroundColor: 'red'}]}>
+      </View>
+
+      <View style={[styles.botoes, {backgroundColor: 'red'}]}>
+      </View>
+
+      <View style={[styles.botoes, {backgroundColor: 'red'}]}>
+      </View>
+
+      <View style={[styles.botoes, {backgroundColor: 'red'}]}>
+      </View>
+
+      <View style={[styles.botoes, {backgroundColor: 'red'}]}>
+      </View>
+
       <StatusBar style="auto" />
     </View>
   );
@@ -17,10 +40,37 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#ffffff',
+    backgroundColor: '#ffffff00',
+    alignItems: 'center',
+  },
+
+  logo: {
+    flexDirection: 'row',
+    width: '100%',
+    height: 100,
     alignItems: 'center',
     justifyContent: 'center',
-    flexDirection: 'row',
-
+    paddingTop: 50,
+    backgroundColor: '#9b0000',
   },
+
+  text: {
+    marginLeft: 10,
+    fontSize: 20,
+    color: 'white',
+  },
+
+  imagem: {
+    width: '100%',
+    height: 200,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+
+  botoes: {
+    width: 350,
+    height: 100,
+    borderRadius: 20,
+    margin: 10
+  }
 });
