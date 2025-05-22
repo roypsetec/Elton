c722a4e4d85179062dc0d916a22d6b50ec15dc56
roypsetec <roypstcc@gmail.com>
2025-05-22 20:27:18 -0300

    clima horizontal

--- AppClima/clima/App.js
+++ AppClima/clima/App.js
@@ -1,23 +1,196 @@
 import { StatusBar } from 'expo-status-bar';
-import { ImageBackground, Text, View } from 'react-native';
+import { ImageBackground, Text, View, Image, ScrollView } from 'react-native';
 import styles from './css';
 import Ionicons from '@expo/vector-icons/Ionicons';
+import Foundation from '@expo/vector-icons/Foundation';
 
 export default function App() {
   return (
-    <View style={styles.container}>
 
-      <ImageBackground source={require('./assets/fundo.jpg')}></ImageBackground>
+    <ImageBackground source={require('./assets/fundo.jpg')} style={styles.fundo}>
 
+      <View style={styles.container}>
 
-      <View style={styles.navbar}>
-      <Ionicons name="menu" size={32}color="white" />
-      <Text style={styles.textNavBar}>Votuporanga</Text>
-      <Ionicons name="location-sharp" size={20} color="white" />
+        <View style={styles.navbar}>
+          <Ionicons name="menu" size={32} color="white" />
+          <Text style={styles.textNavBar}>Votuporanga</Text>
+          <Ionicons name="location-sharp" size={20} color="white" />
+        </View>
+        <View style={styles.hoje}>
+          <Text style={styles.textAgora}>Agora</Text>
+          <View style={styles.clima}>
+            <Text style={{ color: 'white', fontSize: 60, paddingTop: 10, }}>33°</Text>
+            <Image source={require('./assets/sol.png')} style={styles.sol} />
+          </View>
+          <Text style={{ color: 'white', fontSize: 15, paddingTop: 10, }}>Máxima: 34º / Mínima: 17º</Text>
+        </View>
+
+        <View style={styles.alerta}>
+          <Foundation name="alert" size={40} color="#d29599" />
+          <Text style={{ color: '#d29599', paddingLeft: 20, }}>Alerta Laranja, Baixa Umidade</Text>
+        </View>
+
+        <View style={styles.conteudo}>
+
+          <Text style={styles.textAgora}>Previsão do tempo de hora em hora</Text>
+
+          <ScrollView style={styles.climaHorizontal} horizontal>
+            <View style={styles.umclima}>
+              <Text style={styles.textClima}>33º</Text>
+              <Image source={require('./assets/sol.png')} style={styles.iconeClima} />
+              <Text style={styles.textClima}>Agora</Text>
+            </View>
+
+            <View style={styles.umclima}>
+              <Text style={styles.textClima}>31º</Text>
+              <Image source={require('./assets/nublado.png')} style={styles.iconeClima} />
+              <Text style={styles.textClima}>14:00</Text>
+            </View>
+
+            <View style={styles.umclima}>
+              <Text style={styles.textClima}>29º</Text>
+              <Image source={require('./assets/nublado.png')} style={styles.iconeClima} />
+              <Text style={styles.textClima}>15:00</Text>
+            </View>
+
+            <View style={styles.umclima}>
+              <Text style={styles.textClima}>27º</Text>
+              <Image source={require('./assets/chuva.png')} style={styles.iconeClima} />
+              <Text style={styles.textClima}>16:00</Text>
+            </View>
+
+            <View style={styles.umclima}>
+              <Text style={styles.textClima}>27º</Text>
+              <Image source={require('./assets/chuva.png')} style={styles.iconeClima} />
+              <Text style={styles.textClima}>17:00</Text>
+            </View>
+
+            <View style={styles.umclima}>
+              <Text style={styles.textClima}>26º</Text>
+              <Image source={require('./assets/lua.png')} style={styles.iconeClima} />
+              <Text style={styles.textClima}>18:00</Text>
+            </View>
+
+            <View style={styles.umclima}>
+              <Text style={styles.textClima}>25º</Text>
+              <Image source={require('./assets/lua.png')} style={styles.iconeClima} />
+              <Text style={styles.textClima}>19:00</Text>
+            </View>
+
+            <View style={styles.umclima}>
+              <Text style={styles.textClima}>25º</Text>
+              <Image source={require('./assets/lua.png')} style={styles.iconeClima} />
+              <Text style={styles.textClima}>20:00</Text>
+            </View>
+
+            <View style={styles.umclima}>
+              <Text style={styles.textClima}>25º</Text>
+              <Image source={require('./assets/lua.png')} style={styles.iconeClima} />
+              <Text style={styles.textClima}>21:00</Text>
+            </View>
+
+            <View style={styles.umclima}>
+              <Text style={styles.textClima}>23º</Text>
+              <Image source={require('./assets/lua.png')} style={styles.iconeClima} />
+              <Text style={styles.textClima}>22:00</Text>
+            </View>
+
+            <View style={styles.umclima}>
+              <Text style={styles.textClima}>23º</Text>
+              <Image source={require('./assets/lua.png')} style={styles.iconeClima} />
+              <Text style={styles.textClima}>23:00</Text>
+            </View>
+
+            <View style={styles.umclima}>
+              <Text style={styles.textClima}>22º</Text>
+              <Image source={require('./assets/lua.png')} style={styles.iconeClima} />
+              <Text style={styles.textClima}>00:00</Text>
+            </View>
+
+            <View style={styles.umclima}>
+              <Text style={styles.textClima}>22º</Text>
+              <Image source={require('./assets/lua.png')} style={styles.iconeClima} />
+              <Text style={styles.textClima}>01:00</Text>
+            </View>
+
+            <View style={styles.umclima}>
+              <Text style={styles.textClima}>21º</Text>
+              <Image source={require('./assets/lua.png')} style={styles.iconeClima} />
+              <Text style={styles.textClima}>02:00</Text>
+            </View>
+
+            <View style={styles.umclima}>
+              <Text style={styles.textClima}>20º</Text>
+              <Image source={require('./assets/lua.png')} style={styles.iconeClima} />
+              <Text style={styles.textClima}>03:00</Text>
+            </View>
+
+            <View style={styles.umclima}>
+              <Text style={styles.textClima}>19º</Text>
+              <Image source={require('./assets/lua.png')} style={styles.iconeClima} />
+              <Text style={styles.textClima}>04:00</Text>
+            </View>
+
+            <View style={styles.umclima}>
+              <Text style={styles.textClima}>20º</Text>
+              <Image source={require('./assets/lua.png')} style={styles.iconeClima} />
+              <Text style={styles.textClima}>05:00</Text>
+            </View>
+
+            <View style={styles.umclima}>
+              <Text style={styles.textClima}>23º</Text>
+              <Image source={require('./assets/lua.png')} style={styles.iconeClima} />
+              <Text style={styles.textClima}>06:00</Text>
+            </View>
+
+            <View style={styles.umclima}>
+              <Text style={styles.textClima}>24º</Text>
+              <Image source={require('./assets/nublado.png')} style={styles.iconeClima} />
+              <Text style={styles.textClima}>07:00</Text>
+            </View>
+
+            <View style={styles.umclima}>
+              <Text style={styles.textClima}>24º</Text>
+              <Image source={require('./assets/nublado.png')} style={styles.iconeClima} />
+              <Text style={styles.textClima}>08:00</Text>
+            </View>
+
+            <View style={styles.umclima}>
+              <Text style={styles.textClima}>25º</Text>
+              <Image source={require('./assets/nublado.png')} style={styles.iconeClima} />
+              <Text style={styles.textClima}>09:00</Text>
+            </View>
+
+            <View style={styles.umclima}>
+              <Text style={styles.textClima}>26º</Text>
+              <Image source={require('./assets/sol.png')} style={styles.iconeClima} />
+              <Text style={styles.textClima}>10:00</Text>
+            </View>
+
+            <View style={styles.umclima}>
+              <Text style={styles.textClima}>26º</Text>
+              <Image source={require('./assets/sol.png')} style={styles.iconeClima} />
+              <Text style={styles.textClima}>11:00</Text>
+            </View>
+
+            <View style={styles.umclima}>
+              <Text style={styles.textClima}>27º</Text>
+              <Image source={require('./assets/sol.png')} style={styles.iconeClima} />
+              <Text style={styles.textClima}>12:00</Text>
+            </View>
+
+            <View style={styles.umclima}>
+              <Text style={styles.textClima}>27º</Text>
+              <Image source={require('./assets/sol.png')} style={styles.iconeClima} />
+              <Text style={styles.textClima}>13:00</Text>
+            </View>
+          </ScrollView>
+          
+        </View>
+
+        <StatusBar style="auto" />
       </View>
-
-
-      <StatusBar style="auto" />
-    </View>
+    </ImageBackground>
   );
 }
+
--- AppClima/clima/css.js
+++ AppClima/clima/css.js
@@ -3,7 +3,6 @@ import { StyleSheet } from "react-native";
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#790a0a',
   },
   navbar: {
     paddingLeft: 10,
@@ -17,6 +16,64 @@ const styles = StyleSheet.create({
     fontSize: 18,
     paddingRight: 2,
   },
+  fundo: {
+    width: '100%',
+    height: '100%',
+  },
+  hoje: {
+    padding: 20,
+    paddingTop: 40,
+  },
+  textAgora: {
+    color: 'white',
+    fontSize: 15,
+  },
+  clima: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  sol: {
+    width: 80,
+    height: 80,
+    marginLeft: 10,
+  },
+  alerta: {
+    width: 380,
+    height: 70,
+    backgroundColor: '#930000',
+    borderRadius: 10,
+    paddingLeft: 20,
+    alignSelf: 'center',
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  conteudo: {
+    padding: 15,
+  },
+  climaHorizontal: {
+    width: '100%',
+    height: 120,
+    backgroundColor: '#00000046',
+    borderRadius: 15,
+    marginTop: 20,
+  },
+  umclima: {
+    padding: 13,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  iconeClima: {
+    width: 50,
+    height: 40,
+    marginTop: 5,
+    marginBottom: 5,
+  },
+  textClima: {
+    color: 'white',
+    fontSize: 16,
+
+  }
+
 });
 
 export default styles;
