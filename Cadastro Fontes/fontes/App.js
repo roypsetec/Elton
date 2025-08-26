aa2b7faf9f937a8b886d4bc35cfdec9cf775a976
roypsetec <roypstcc@gmail.com>
2025-08-26 12:01:34 -0300

    elton

--- Cadastro Fontes/fontes/App.js
+++ Cadastro Fontes/fontes/App.js
@@ -1,10 +1,12 @@
-import { Text, SafeAreaView, StyleSheet, View, TextInput } from 'react-native';
+import { Text, SafeAreaView, StyleSheet, View, TextInput, TouchableOpacity } from 'react-native';
 import { useFonts } from 'expo-font';
 
 export default function App() {
 
   const [fontsLoaded] = useFonts({
     Fonte1: require('./assets/fonts/fonte1.ttf'),
+    Fonte2: require('./assets/fonts/fonte2.ttf'),
+    Fonte3: require('./assets/fonts/fonte3.otf'),
 
   });
 
@@ -27,39 +29,56 @@ export default function App() {
         <Text style={styles.txtcampos}>Nome</Text>
         <TextInput
           style={styles.input}
-          placeholder="Nome"
+          placeholder="Seu nome"
           placeholderTextColor="#aaa"
         />
 
+        <Text style={styles.txtcampos}>Email</Text>
         <TextInput
           style={styles.input}
-          placeholder="Email"
+          placeholder="Seu@email.com"
           placeholderTextColor="#aaa"
           keyboardType="email-address"
         />
 
+        <Text style={styles.txtcampos}>Telefone</Text>
         <TextInput
           style={styles.input}
-          placeholder="Telefone"
+          placeholder="(00) 00000-0000"
           placeholderTextColor="#aaa"
           keyboardType="phone-pad"
         />
 
+        <Text style={styles.txtcampos}>Senha</Text>
         <TextInput
           style={styles.input}
-          placeholder="Senha"
+          placeholder="*****"
           placeholderTextColor="#aaa"
           secureTextEntry
         />
 
+        <Text style={styles.txtcampos}>Confirmar Senha</Text>
         <TextInput
           style={styles.input}
-          placeholder="Confirmar senha"
+          placeholder="*****"
           placeholderTextColor="#aaa"
           secureTextEntry
         />
+
+        <TouchableOpacity style={styles.botao} onPress={() => alert('Você criou sua conta!')} >
+          <Text style={styles.txtcriar}>Criar conta</Text>
+        </TouchableOpacity>
+
       </View>
 
+      <View style={styles.logar}>
+        <Text style={styles.txtja}>
+          Já tem uma conta?{' '}
+          <Text style={styles.txtlogin} onPress={() => alert('Página em desenvolvimento!')} >
+            Login
+          </Text>
+        </Text>
+      </View>
 
     </SafeAreaView>
   );
@@ -68,7 +87,6 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-
     backgroundColor: '#1B4332',
 
   },
@@ -92,17 +110,50 @@ const styles = StyleSheet.create({
   txtcadastro: {
     fontSize: 50,
     fontFamily: 'Fonte1',
-    color: '#8B4513'
+    color: '#da7127'
   },
   subtxt: {
-    color: '#F5F5DC'
+    color: '#b1b1ae',
+    marginBottom: 25,
+    fontSize: 16
   },
   input: {
     backgroundColor: "#00000050",
     padding: 12,
-    marginVertical: 15,
+    marginBottom: 10,
+    marginTop: 5,
     borderRadius: 10,
     fontSize: 16,
-    height: 50
+    height: 60,
+    color: "white",
+    fontFamily: 'Fonte3'
   },
+  txtcampos: {
+    color: 'white',
+  },
+  botao: {
+    backgroundColor: '#da7127',
+    widht: '100%',
+    height: 70,
+    borderRadius: 15,
+    marginTop: 20,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginBottom: 10,
+  },
+  txtcriar: {
+    fontSize: 36,
+    color: '#1B4332',
+    fontFamily: 'Fonte2',
+  },
+  logar: {
+    alignItems: 'center',
+  },
+  txtja: {
+    color: '#b1b1ae',
+  },
+  txtlogin: {
+    color: '#ffffff'
+  },
+
 });
