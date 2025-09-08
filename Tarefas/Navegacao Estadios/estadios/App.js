70831c196591482d8f54bd14b565f4dae92d2be9
roypsetec <roypstcc@gmail.com>
2025-09-08 14:39:22 -0300

    estadios pronto

--- Tarefas/Navegacao Estadios/estadios/App.js
+++ Tarefas/Navegacao Estadios/estadios/App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { ScrollView, StyleSheet, Text, View, Button, TouchableOpacity, Image} from 'react-native';
+import { ScrollView, StyleSheet, Text, View, Button, TouchableOpacity, Image } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { useFonts } from 'expo-font';
@@ -21,10 +21,12 @@ export default function App() {
 
   return (
     <NavigationContainer>
-      <Stack.Navigator screenOptions={{ headerStyle: { backgroundColor: 'orange', }, headerTintColor: 'white', }}>
+      <Stack.Navigator screenOptions={{ headerStyle: { backgroundColor: '#809630', }, headerTintColor: 'white', }}>
 
         <Stack.Screen name="estadios" component={Estadios} options={{ title: "Estadios pelo Mundo" }} />
-        <Stack.Screen name="jose" component={Jose} options={{ title:"" }} />
+        <Stack.Screen name="neoquimica" component={Neo} options={{ title: "Neo Química Arena" }} />
+        <Stack.Screen name="allianz" component={Allianz} options={{ title: "Allianz Arena" }} />
+        <Stack.Screen name="emirates" component={Emirates} options={{ title: "Emirates Stadium" }} />
 
       </Stack.Navigator>
     </NavigationContainer>
@@ -36,28 +38,144 @@ function Estadios({ navigation }) {
     <View style={styles.home}>
       <Text style={styles.bemvindo}>Bem-vindo ao Guia de Estádios pelo mundo</Text>
 
-      <TouchableOpacity onPress={() => navigation.navigate('jose')}>
-        <Image source={require('./assets/estadios/neo1.jpg')}></Image>
+      <TouchableOpacity onPress={() => navigation.navigate('neoquimica')} style={styles.viewEstadios}>
+        <Image source={require('./assets/estadios/neo/neo1.jpg')} style={styles.imgEstadios}></Image>
+        <Text style={styles.txtEstadios}>Neo Química Arena</Text>
       </TouchableOpacity>
+
+      <TouchableOpacity onPress={() => navigation.navigate('allianz')} style={styles.viewEstadios}>
+        <Image source={require('./assets/estadios/bayer/bay1.jpg')} style={styles.imgEstadios}></Image>
+        <Text style={styles.txtEstadios}>Neo Química Arena</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity onPress={() => navigation.navigate('emirates')} style={styles.viewEstadios}>
+        <Image source={require('./assets/estadios/arsenal/arse1.jpg')} style={styles.imgEstadios}></Image>
+        <Text style={styles.txtEstadios}>Neo Química Arena</Text>
+      </TouchableOpacity>
+
+
 
     </View>
   )
 }
 
-function Jose({ navigation }) {
+function Neo({ navigation }) {
   return (
-    <View>
-      <Text> Você acessou o app José </Text>
-    </View>
+    <ScrollView style={styles.neoquimica}>
+      <Image source={require('./assets/estadios/neo/neo1.jpg')} style={styles.imgGrande}></Image>
+
+
+      <View style={styles.imagens}>
+
+        <Text style={styles.txtApresenta}>No coração de São Paulo, pulsa a alma do Timão.</Text>
+
+        <Image source={require('./assets/estadios/neo/neo2.jpg')} style={styles.imgPequena}></Image>
+
+        <Image source={require('./assets/estadios/neo/neo3.jpg')} style={styles.imgPequena}></Image>
+
+        <Image source={require('./assets/estadios/neo/neo4.jpeg')} style={styles.imgPequena}></Image>
+
+        <Image source={require('./assets/estadios/neo/neo5.jpg')} style={styles.imgPequena}></Image>
+      </View>
+
+    </ScrollView>
+  )
+}
+
+function Allianz({ navigation }) {
+  return (
+    <ScrollView style={styles.neoquimica}>
+      <Image source={require('./assets/estadios/bayer/bay1.jpg')} style={styles.imgGrande}></Image>
+
+
+      <View style={styles.imagens}>
+
+        <Text style={styles.txtApresenta}>Na Allianz, o Bayern respira sua força e união.</Text>
+
+        <Image source={require('./assets/estadios/bayer/bay2.jpg')} style={styles.imgPequena}></Image>
+
+        <Image source={require('./assets/estadios/bayer/bay3.jpg')} style={styles.imgPequena}></Image>
+
+        <Image source={require('./assets/estadios/bayer/bay4.jpg')} style={styles.imgPequena}></Image>
+
+        <Image source={require('./assets/estadios/bayer/bay5.jpg')} style={styles.imgPequena}></Image>
+      </View>
+
+    </ScrollView>
+  )
+}
+
+function Emirates({ navigation }) {
+  return (
+    <ScrollView style={styles.neoquimica}>
+      <Image source={require('./assets/estadios/arsenal/arse1.jpg')} style={styles.imgGrande}></Image>
+
+
+      <View style={styles.imagens}>
+
+        <Text style={styles.txtApresenta}>No Emirates, o Arsenal veste sua história.</Text>
+
+        <Image source={require('./assets/estadios/arsenal/arse2.jpg')} style={styles.imgPequena}></Image>
+
+        <Image source={require('./assets/estadios/arsenal/arse3.jpg')} style={styles.imgPequena}></Image>
+
+        <Image source={require('./assets/estadios/arsenal/arse4.jpg')} style={styles.imgPequena}></Image>
+
+        <Image source={require('./assets/estadios/arsenal/arse5.jpg')} style={styles.imgPequena}></Image>
+      </View>
+
+    </ScrollView>
   )
 }
 
 const styles = StyleSheet.create({
   home: {
+    flex: 1,
+    backgroundColor: '#82c08c',
     alignItems: 'center'
   },
   bemvindo: {
     fontSize: 17,
     fontFamily: 'Fonte2',
+    marginTop: 10,
+    marginBottom: 20,
+  },
+  viewEstadios: {
+    width: '90%',
+    elevation: 5,
+    marginVertical: 10,
+    backgroundColor: 'white',
+    borderRadius: 12,
+  },
+  imgEstadios: {
+    width: '100%',
+    height: 180,
+    borderTopLeftRadius: 12,
+    borderTopRightRadius: 12,
+  },
+  txtEstadios: {
+    padding: 7,
+    fontWeight: 'bold'
+  },
+  imgGrande: {
+    width: '100%',
+    height: 250
+  },
+  txtApresenta: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    marginTop: 20,
+    marginBottom: 40
+  },
+  neoquimica: {
+  },
+  imgPequena: {
+    width: '90%',
+    height: 200,
+    borderRadius: 10,
+    marginVertical: 10,
+  },
+  imagens: {
+    alignItems: 'center',
   },
 });
